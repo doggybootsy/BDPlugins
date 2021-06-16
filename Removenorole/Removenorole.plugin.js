d3f8ff654cb20bcb3eb3b30b7da350d5df588513
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2021-06-15 20:20:16 -0700

    DONE, no console spam and works

--- Removenorole/Removenorole.plugin.js
+++ Removenorole/Removenorole.plugin.js
@@ -2,9 +2,10 @@
  * @name Removenorole
  * @author Doggybootsy
  * @description Remove the "NO ROLES" from user popouts
- * @version 1.1
+ * @version 1.2
+ * @source https://github.com/doggybootsy/BDPlugins/blob/main/Removenorole/Removenorole.plugin.js
+ * @website https://doggybootsy.github.io/
  */
-
 
 module.exports = class Remove_no_role{
     start() {
@@ -14,10 +15,13 @@ module.exports = class Remove_no_role{
            document.querySelector('#app-mount>div+.layerContainer-yqaFcK[Removenorole]').addEventListener('DOMSubtreeModified', Remove_no_role, false);
         }
         function Remove_no_role() {
-            const remove_no_role = document.querySelector('[Removenorole] .userPopout-xaxa6l .root-3-B5F3:empty')
-            if (!remove_no_role) return;
-            remove_no_role.previousSibling.style.display = "none"
-            remove_no_role.style.display = "none"
+            if (document.querySelector('#app-mount>div+.layerContainer-yqaFcK').innerHTML.includes('userPopout-')) {
+
+                const remove_no_role = document.querySelector('[Removenorole] .userPopout-xaxa6l .root-3-B5F3:empty')
+                if (!remove_no_role) return;
+                document.querySelector('[Removenorole] .userPopout-xaxa6l .root-3-B5F3:empty').previousSibling.style.display = "none"
+                document.querySelector('[Removenorole] .userPopout-xaxa6l .root-3-B5F3:empty').style.display = "none"
+            }
         }
     }
     stop() {
