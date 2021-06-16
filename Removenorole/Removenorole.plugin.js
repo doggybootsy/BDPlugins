fb37ce4d58ab9e66020de20a700f2971fd66cd49
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2021-06-15 20:31:31 -0700

    removed annoying stuff

--- Removenorole/Removenorole.plugin.js
+++ Removenorole/Removenorole.plugin.js
@@ -11,12 +11,10 @@ module.exports = class Remove_no_role{
     start() {
         document.querySelector('#app-mount>div+.layerContainer-yqaFcK').setAttribute('Removenorole','')
         if(window.addEventListener) {
-           // Normal browsers
            document.querySelector('#app-mount>div+.layerContainer-yqaFcK[Removenorole]').addEventListener('DOMSubtreeModified', Remove_no_role, false);
         }
         function Remove_no_role() {
             if (document.querySelector('#app-mount>div+.layerContainer-yqaFcK').innerHTML.includes('userPopout-')) {
-
                 const remove_no_role = document.querySelector('[Removenorole] .userPopout-xaxa6l .root-3-B5F3:empty')
                 if (!remove_no_role) return;
                 document.querySelector('[Removenorole] .userPopout-xaxa6l .root-3-B5F3:empty').previousSibling.style.display = "none"
