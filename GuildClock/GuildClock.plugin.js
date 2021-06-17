1eb63bb38286bbfb2c43d3fca77e260e5718ca62
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2021-06-16 18:08:11 -0700

    Add copy

--- GuildClock/GuildClock.plugin.js
+++ GuildClock/GuildClock.plugin.js
@@ -7,16 +7,21 @@
 
 module.exports = class GuildClock{
     start() {
-        // Css
-        BdApi.injectCSS("GuildClock", '#guildclock{color: var(--text-normal); text-align: center; padding: 6px 0; font-size: 1rem}#guildclock::after{content: " " attr(pm_am);}')
+        // Css | Can easily get changed to other apis
+        BdApi.injectCSS(
+            "GuildClock",
+            '#guildclock{color: var(--text-normal); text-align: center; font-size: 1rem} #guildclock::after{content: " " attr(pm_am);}'
+        );
         // Add Attribute
         document.getElementsByClassName('guilds-1SWlCJ')[0].setAttribute('guildclock','');
         // Remove extra guildclock element
         if(document.getElementsByClassName('guilds-1SWlCJ')[0].innerHTML.includes('id="guildclock"')){
-            document.getElementById('guildclock').remove()
+            document.querySelectorAll('#guildclock').forEach(e => {
+                e.remove();
+            });
         }
         // Create Element
-        document.querySelector('[guildclock] .tutorialContainer-2sGCg9').insertAdjacentHTML('afterend', '<div id="guildclock"></div>');
+        document.querySelector('[guildclock] .tutorialContainer-2sGCg9').insertAdjacentHTML('afterend', '<div id="guildclock" class="listItem-GuPuDH"></div>');
         // Time stuff
         function GuildClock_time_stuff(){
             const GuildClock_time = new Date();
@@ -25,19 +30,47 @@ module.exports = class GuildClock{
             // Insert time
             document.getElementById('guildclock').innerText = GuildClock_time_Hour+':'+GuildClock_time_Minute;
             if(GuildClock_time.getHours() >= 12 ? "PM" : "AM" == 'PM'){
-                document.getElementById('guildclock').setAttribute('pm_am','pm')
+                document.getElementById('guildclock').setAttribute('pm_am','pm');
             } else{
-                document.getElementById('guildclock').setAttribute('pm_am','am')
+                document.getElementById('guildclock').setAttribute('pm_am','am');
             }
         }
-        setInterval(GuildClock_time_stuff, 1000);
+        setInterval(GuildClock_time_stuff, 10000);
+        // Add copy time
+        setTimeout(function(){
+            document.querySelector('#guildclock').addEventListener("click", () => {
+                const GuildClock_copy_string = document.getElementById('guildclock').innerText+document.getElementById('guildclock').getAttribute('pm_am')
+                const GuildClock_copy_element = document.createElement('textarea');
+                GuildClock_copy_element.value = GuildClock_copy_string;
+                GuildClock_copy_element.setAttribute('readonly', '');
+                GuildClock_copy_element.style.position = 'absolute';
+                GuildClock_copy_element.style.left = '-9999px';
+                document.body.appendChild(GuildClock_copy_element);
+                GuildClock_copy_element.select();
+                document.execCommand('copy');
+                document.body.removeChild(GuildClock_copy_element);
+            })
+        }, 1000);
     }
     stop() {
         // Remove Attribute
         document.getElementsByClassName('guilds-1SWlCJ')[0].removeAttribute('guildclock');
         // hide Element
-        document.getElementById('guildclock').style.display = "none"
+        document.getElementById('guildclock').style.display = "none";
         // Remove css
-        BdApi.injectCSS("GuildClock", '')
+        document.querySelector('style#GuildClock').remove()
+        // remove copy time
+        document.querySelector('#guildclock').removeEventListener("click", () => {
+            const GuildClock_copy_string = document.getElementById('guildclock').innerText+document.getElementById('guildclock').getAttribute('pm_am')
+            const GuildClock_copy_element = document.createElement('textarea');
+            GuildClock_copy_element.value = GuildClock_copy_string;
+            GuildClock_copy_element.setAttribute('readonly', '');
+            GuildClock_copy_element.style.position = 'absolute';
+            GuildClock_copy_element.style.left = '-9999px';
+            document.body.appendChild(GuildClock_copy_element);
+            GuildClock_copy_element.select();
+            document.execCommand('copy');
+            document.body.removeChild(GuildClock_copy_element);
+        })
     }
 }
