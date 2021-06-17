ae80b2a457c4f5bfbfaba0367f33704154087692
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2021-06-16 19:11:53 -0700

    Update Removenorole.plugin.js

--- Removenorole/Removenorole.plugin.js
+++ Removenorole/Removenorole.plugin.js
@@ -1,13 +1,17 @@
 /**
- * @name Removenorole
+ * @name RemoveNoRole
  * @author Doggybootsy
  * @description Remove the "NO ROLES" from user popouts
  * @version 1.2
- * @source https://github.com/doggybootsy/BDPlugins/blob/main/Removenorole/Removenorole.plugin.js
+ * @source https://github.com/doggybootsy/BDPlugins/
  * @website https://doggybootsy.github.io/
  */
 
 module.exports = class Remove_no_role{
+    getName() {
+      return "Remove No Role";
+    }
+
     start() {
         document.querySelector('#app-mount>div+.layerContainer-yqaFcK').setAttribute('Removenorole','')
         if(window.addEventListener) {
@@ -15,8 +19,6 @@ module.exports = class Remove_no_role{
         }
         function Remove_no_role() {
             if (document.querySelector('#app-mount>div+.layerContainer-yqaFcK').innerHTML.includes('userPopout-')) {
-                const remove_no_role = document.querySelector('[Removenorole] .userPopout-xaxa6l .root-3-B5F3:empty')
-                if (!remove_no_role) return;
                 document.querySelector('[Removenorole] .userPopout-xaxa6l .root-3-B5F3:empty').previousSibling.style.display = "none"
                 document.querySelector('[Removenorole] .userPopout-xaxa6l .root-3-B5F3:empty').style.display = "none"
             }
