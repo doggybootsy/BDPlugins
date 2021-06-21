2007a5e1c918a42844753a8d5f64603887380605
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2021-06-20 19:10:49 -0700

    Update Removenorole.plugin.js

--- Removenorole/Removenorole.plugin.js
+++ Removenorole/Removenorole.plugin.js
@@ -2,29 +2,24 @@
  * @name RemoveNoRole
  * @author Doggybootsy
  * @description Remove the "NO ROLES" from user popouts
- * @version 1.3
+ * @version 1.4
  * @source https://github.com/doggybootsy/BDPlugins/
  * @website https://doggybootsy.github.io/
  */
 
 module.exports = class Remove_no_role{
     getName() {
-      return "Remove No Role";
+        return "Remove No Role";
     }
-    start() {
-        document.querySelector('#app-mount>div+.layerContainer-yqaFcK').setAttribute('removenorole','')
-        if(window.addEventListener) {
-           // Normal browsers
-           document.querySelector('#app-mount>div+.layerContainer-yqaFcK[removenorole]').addEventListener('DOMSubtreeModified', Remove_no_role, false);
-        }
-        function Remove_no_role() {
-            if (document.body.contains(document.querySelector('[removenorole] .root-3-B5F3:empty'))) {
-                document.querySelector('[removenorole] .userPopout-xaxa6l .root-3-B5F3:empty').previousSibling.setAttribute('style','display: none !important')
-                document.querySelector('[removenorole] .userPopout-xaxa6l .root-3-B5F3:empty').setAttribute('style','display: none !important')
-            }
-        }
-    }
-    stop() {
-        document.querySelector('#app-mount>div+.layerContainer-yqaFcK').removeAttribute('removenorole')
-    }
+
+    observer (mutations) {
+        const Remove_no_role_selector = document.querySelector('.userPopout-xaxa6l .root-3-B5F3:empty');
+        if (document.body.contains(Remove_no_role_selector)) {
+            Remove_no_role_selector.previousSibling.remove();
+            Remove_no_role_selector.remove();
+        };
+    };
+
+    start() {};
+    stop() {};
 }
