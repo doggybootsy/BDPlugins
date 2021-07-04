eb81833ee8f3f243378a7bd08c3bec3a4b02f39e
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2021-07-03 23:11:36 -0700

    Update Removenorole.plugin.js

--- Removenorole/Removenorole.plugin.js
+++ Removenorole/Removenorole.plugin.js
@@ -7,15 +7,19 @@
  * @website https://doggybootsy.github.io/
  */
 
-module.exports = class Remove_no_role{
+ module.exports = class Remove_no_role{
     getName() {
         return "Remove No Role";
     }
 
     observer (mutations) {
         if (document.querySelector('.userPopout-xaxa6l .root-3-B5F3:empty')) {
-            document.querySelector('.userPopout-xaxa6l .root-3-B5F3:empty').previousSibling.remove();
-            document.querySelector('.userPopout-xaxa6l .root-3-B5F3:empty').remove();
+            document.querySelector('.userPopout-xaxa6l .root-3-B5F3:empty').previousSibling.style.display = "none";
+            document.querySelector('.userPopout-xaxa6l .root-3-B5F3:empty').style.display = "none";
+        };
+        if (document.querySelector('.userPopout-xaxa6l .root-3-B5F3[style]:not(:empty)')) {
+            document.querySelector('.userPopout-xaxa6l .root-3-B5F3[style]:not(:empty)').previousSibling.removeAttribute('style')
+            document.querySelector('.userPopout-xaxa6l .root-3-B5F3[style]:not(:empty)').removeAttribute('style')
         };
     };
 
