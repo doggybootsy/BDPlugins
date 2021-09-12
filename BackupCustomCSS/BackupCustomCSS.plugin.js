35b79ec6f8ef75e4a2b983b845014d570ad6075d
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2021-09-12 10:00:32 -0700

    eeeeeeeee

--- BackupCustomCSS/BackupCustomCSS.plugin.js
+++ BackupCustomCSS/BackupCustomCSS.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name BackupCustomCSS
  * @description Back up custom css in a seperate file. Hold shift and tap the save icon to open settings
- * @version 1.2.5
+ * @version 1.2.6
  * @author doggybootsy
  * @source https://github.com/doggybootsy/BDPlugins/
  * @website https://doggybootsy.github.io/
@@ -19,10 +19,10 @@ let {env: {DISCORD_RELEASE_CHANNEL}, platform} = process,
     {openModal} = BdApi.findModuleByProps("openModal"),
     {DONE, DELETE, CANCEL} = BdApi.findModuleByProps("Messages").Messages,
     TextInput = BdApi.findModule(m=>m?.defaultProps?.type==="text"),
-    folder = loadData("BackupCustomCSS", "saveLocation") ?? Plugins.folder,
     {FormItem, FormText} = BdApi.findModuleByProps("FormItem", "FormText"),
     dummmypath = platform === "win32" ? Plugins.folder.split("\\") : Plugins.folder.split("/"),
-    markdown = BdApi.findModuleByDisplayName("Markdown")
+    setData = (d,v) => saveData("BackupCustomCSS", d, v),
+    getData = d => loadData("BackupCustomCSS", d)
 class Switch extends Component {
     constructor(props) {
         super(props)
@@ -35,7 +35,7 @@ class Switch extends Component {
             note: this.props.note,
             onChange: (value) => {
                 this.setState({ toggled: value })
-                saveData("BackupCustomCSS", this.props.id, value)
+                setData(this.props.id, value)
                 if(document.getElementById("BackupCustomCSS")) document.getElementById("BackupCustomCSS").remove()
             }
         })
@@ -71,7 +71,7 @@ class Group extends Component {
 class SettingsPanel extends Component {
     constructor() {
         super(...arguments)
-        this.state = {Path: folder, ResetButtonHover: false}
+        this.state = {Path: getData("saveLocation") ?? Plugins.folder, ResetButtonHover: false}
     }
     render() {
         return createElement("div", {
@@ -98,19 +98,19 @@ class SettingsPanel extends Component {
                     children: [
                         createElement(Switch, {
                             id: "QuickSettings",
-                            value: loadData("BackupCustomCSS", "QuickSettings") ?? true, 
+                            value: getData("QuickSettings") ?? true, 
                             children: "Open settings keybind",
                             note: "Quickly open the settings page"
                         }),
                         createElement(Switch, {
                             id: "QuickDelete",
-                            value: loadData("BackupCustomCSS", "QuickDelete") ?? true, 
+                            value: getData("QuickDelete") ?? true, 
                             children: "Delete backup's keybind",
                             note: "Quickly open the delete backup's promp"
                         }),
                         createElement(Switch, {
                             id: "QuickOpen",
-                            value: loadData("BackupCustomCSS", "QuickOpen") ?? true, 
+                            value: getData("QuickOpen") ?? true, 
                             children: "Open backups keybind",
                             note: "Quickly open the backups folder"
                         })
@@ -132,7 +132,7 @@ class SettingsPanel extends Component {
                                             value: this.state.Path,
                                             onChange: e => {
                                                 this.setState({Path: e})
-                                                saveData("BackupCustomCSS", "saveLocation", e)
+                                                setData("saveLocation", e)
                                             }
                                         }),
                                         createElement(FormText, {
@@ -160,7 +160,6 @@ class SettingsPanel extends Component {
                 createElement("div", {
                     style: {display: "flex"},
 					children: [
-                        createElement("div", {style: {width: "28rem"}}),
                         createElement(Button.default, {
 							color: ButtonColors.RED,
                             look: ButtonLooks.OUTLINED,
@@ -176,12 +175,13 @@ class SettingsPanel extends Component {
 module.exports = class BackupCustomCSS{
     getName() {return "Backup custom CSS"}
     checkIf() {
+        const folder = getData("saveLocation") ?? Plugins.folder
         try {
             if(!existsSync(join(folder, "BackupCustomCSS"))) mkdirSync(join(folder, "BackupCustomCSS"))
             if(!existsSync(join(folder, "BackupCustomCSS", DISCORD_RELEASE_CHANNEL))) mkdirSync(join(folder, "BackupCustomCSS", DISCORD_RELEASE_CHANNEL))
         } catch (err) {
             this._error("You have entered a invalid path, the path has been reset.", err)
-            saveData("BackupCustomCSS", "saveLocation", Plugins.folder)
+            setData("saveLocation", Plugins.folder)
         }
     }
     getSettingsPanel() {return createElement(SettingsPanel, {instance: this})}
@@ -203,7 +203,7 @@ module.exports = class BackupCustomCSS{
         readFile(join(Plugins.folder, "..", "data", DISCORD_RELEASE_CHANNEL, "custom.css"), "utf8", (err, data) => {
             if(err) this.error("Couldnt read custom css", err)
             else {
-                const date = new Date(), time = `${date}`.split(" ")
+                const date = new Date(), time = `${date}`.split(" "), folder = getData("saveLocation") ?? Plugins.folder
                 writeFile(join(folder, "BackupCustomCSS", DISCORD_RELEASE_CHANNEL, `BackupCustomCSS-(${time[0]} ${time[1]} ${time[2]}-${time[3]}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}-${date.getMilliseconds()}).css`), `/*\n    Backup data\n    time: ${date}\n    UnixTimestamp: ${date.getTime()}\n*/\n${data}`, function (err) {
                     if(err) this.error("Couldnt backup css", err)
                     else showToast("CSS successfully backed up", {type:"info", icon: true})
@@ -217,6 +217,7 @@ module.exports = class BackupCustomCSS{
                 confirmText: DELETE,
                 cancelText: CANCEL,
                 onConfirm: () => {
+                    const folder = getData("saveLocation") ?? Plugins.folder
                     rmdir(join(folder, "BackupCustomCSS", DISCORD_RELEASE_CHANNEL), { recursive: true }, (err) => {
                         if(err) this.error("Couldnt delete the css", err)
                         else showToast("Successfully deleted backup(s)", {type:"warning", icon: true})
@@ -238,9 +239,10 @@ module.exports = class BackupCustomCSS{
             ele.classList = "btn btn-primary"
             ele.innerHTML = "<svg viewBox=\"0 0 30 30\" style=\"width: 18px; height: 18px;\"><path d=\"M 7 2 C 5.895 2 5 2.895 5 4 L 5 18 C 5 19.105 5.895 20 7 20 L 23 20 C 24.105 20 25 19.105 25 18 L 25 4 C 25 2.895 24.105 2 23 2 L 7 2 z M 15 5 C 18.314 5 21 7.686 21 11 C 21 14.314 18.314 17 15 17 C 13.748245 17 12.586657 16.614505 11.625 15.958984 L 12.878906 14.078125 C 13.296906 13.451125 12.550828 12.703094 11.923828 13.121094 L 10.041016 14.376953 C 9.3849538 13.415043 9 12.252319 9 11 C 9 7.686 11.686 5 15 5 z M 15 10 C 14.448 10 14 10.448 14 11 C 14 11.552 14.448 12 15 12 C 15.552 12 16 11.552 16 11 C 16 10.448 15.552 10 15 10 z M 5 21.443359 L 5 26 C 5 27.105 5.895 28 7 28 L 23 28 C 24.105 28 25 27.105 25 26 L 25 21.443359 C 24.409 21.787359 23.732 22 23 22 L 7 22 C 6.268 22 5.591 21.787359 5 21.443359 z M 22 24 C 22.552 24 23 24.448 23 25 C 23 25.552 22.552 26 22 26 C 21.448 26 21 25.552 21 25 C 21 24.448 21.448 24 22 24 z\"></path></svg>"
             ele.onclick = (e) => {
-                if(e.shiftKey === true && loadData("BackupCustomCSS", "QuickSettings") === true) this.showSettingsModal()
-                else if(e.ctrlKey === true || e.metaKey === true && loadData("BackupCustomCSS", "QuickOpen") === true) openPath(join(folder, "BackupCustomCSS", DISCORD_RELEASE_CHANNEL))
-                else if(e.altKey === true && loadData("BackupCustomCSS", "QuickDelete") === true) this.removeAll()
+                const folder = getData("saveLocation") ?? Plugins.folder
+                if(e.shiftKey === true && getData("QuickSettings") === true) this.showSettingsModal()
+                else if(e.ctrlKey === true || e.metaKey === true && getData("QuickOpen") === true) openPath(join(folder, "BackupCustomCSS", DISCORD_RELEASE_CHANNEL))
+                else if(e.altKey === true && getData("QuickDelete") === true) this.removeAll()
                 else this.backup()
             }
             document.querySelector("#bd-editor-controls>.controls-section.controls-left").appendChild(ele)
