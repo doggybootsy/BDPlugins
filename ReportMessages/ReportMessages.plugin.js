9150ea61c35f385aec8da6d157a967af137369b0
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2022-03-31 19:31:52 -0700

    There

--- ReportMessages/ReportMessages.plugin.js
+++ ReportMessages/ReportMessages.plugin.js
@@ -12,48 +12,113 @@ const {
 } = BdApi.findModuleByProps("showReportModalForMessage", "showReportModalForGuild")
 const Alert = BdApi.findModuleByDisplayName("Alert")
 const { openModal } = BdApi.findModuleByProps("openModalLazy", "openModal")
-const showedZlibPrompt = BdApi.getData("ReportMessages", "showedZlibPrompt")
-const showedPrompt = BdApi.getData("ReportMessages", "showedZlibPrompt")
-function alert(...body) {
-  openModal(props => BdApi.React.createElement(Alert, {
-    ...props, title: "ReportMessages", body
-  }))
-}
+const getData = BdApi.getData.bind(this, "ReportMessages")
+const setData = BdApi.saveData.bind(this, "ReportMessages")
+const showedPrompt = getData("showedPrompt")
+const Switch = BdApi.findModuleByDisplayName("SwitchItem")
+const Tooltip = BdApi.findModuleByDisplayName("Tooltip")
+const Markdown = BdApi.findModule((m) => m?.displayName === "Markdown" && m.rules)
+const { React } = BdApi
+// Little i18n ig
+const { Messages } = BdApi.findAllModules(e => e.Messages)[1]
 
 module.exports = class ReportMessages {
+  getName() { return Messages.REPORT_MESSAGE_MENU_OPTION }
   get enabled() { return BdApi.Plugins.isEnabled("ReportMessages") }
-  // use load and always stay patched because I use an async patch (and idk if theres a better way)
+  alert(...body) {
+    openModal(props => React.createElement(Alert, {
+      ...props, title: "ReportMessages", 
+      body: body.map((c) => typeof(c) === "string" ? React.createElement(Markdown, {}, c) : c)
+    }))
+  }
   async load() {
-    // set the shown zlib prompt to 'true'
-    BdApi.saveData("ReportMessages", "showedZlibPrompt", true)
-    BdApi.saveData("ReportMessages", "showedPrompt", true)
-    // show warning
-    if (!showedPrompt) alert("Spam reporting messages can get you banned.")
-    // patch 'MiniPopover' and enable the button
-    BdApi.Patcher.after("ReportMessages", MiniPopover, "default", (_, args) => {
-      if (!Boolean(args[0].children[args[0].children.length - 1]?.props?.message)) return 
-      const ele = args[0].children[args[0].children.length - 1]
-      if (!ele) return
-      ele.props.canReport = this.enabled
-    })
+    // set the shown prompt to 'true'
+    setData("showedPrompt", true)
     // if no zlib prompt
-    if (!window.ZLibrary?.DCM && !showedZlibPrompt) return alert("Install Zlibrary for more features.")
+    if (!window.ZLibrary?.DCM && !showedPrompt) return this.alert("Install **Zlibrary** for more features.", "**Warning**: Spam reporting can get you banned.")
+    // show warning
+    if (!showedPrompt) this.alert("Spam reporting can get you banned.")
+  }
+  // So it doesnt error
+  async start() {
+    // patch 'MiniPopover' and enable the button
+    BdApi.Patcher.after("ReportMessages", MiniPopover, "default", (_, args, res) => {
+      if (!args[0].children[args[0].children.length - 1]?.props?.message) return 
+      const child = res.props.children.find(e => e)
+      if (child) {
+        const oldType = child.type
+        child.type = (...args) => {
+          const res = Reflect.apply(oldType, this, args)
+          if (getData("MP") ?? true) res.props.children.unshift(
+            React.createElement(Tooltip, {
+              text: Messages.REPORT_MESSAGE_MENU_OPTION,
+              children: (ttProps) => React.createElement(MiniPopover.Button, {
+                ...ttProps,
+                children: React.createElement(Flag),
+                onClick: () => showReportModalForMessage(child.props.message)
+              })
+            })
+          )
+          return res
+        }
+      }
+      const ele = args[0].children[args[0].children.length - 1]
+      if (ele) ele.props.canReport = getData("MCN") ?? true
+    })
+    // If no zlib return
+    if (!window.ZLibrary?.DCM && this.hasStarted) return
+    this.hasStarted = true
     // If zlib add the 'MessageContextMenu'
     const MessageContextMenu = await ZLibrary.DCM.getDiscordMenu("MessageContextMenu")
     BdApi.Patcher.after("ReportMessages", MessageContextMenu, "default", (_, [args], res) => {
-      if (!this.enabled) return
+      if (!this.enabled || !(getData("MCN") ?? true)) return
       const ifDevmode = res.props.children.find(e => e.props?.children?.props?.id === "devmode-copy-id")
-      res.props.children.splice(res.props.children.length - (ifDevmode ? 2 : 0), 0, 
+      res.props.children.splice(ifDevmode ? res.props.children.indexOf(ifDevmode) : res.props.children.length, 0, 
         ZLibrary.DCM.buildMenuItem({
           label: "Report Message",
           danger: true,
           action: () => showReportModalForMessage(args.message),
-          icon: () => BdApi.React.createElement(Flag)
+          icon: () => React.createElement(Flag)
         })
       )
     })
   }
-  // So it doesnt error
-  start() {}
-  stop() {}
+  stop() { BdApi.Patcher.unpatchAll("ReportMessages") }
+  getSettingsPanel() {
+    return React.createElement(() => {
+      const [MCN, setMCN] = React.useState(getData("MCN") ?? true)
+      const [mn, setMN] = React.useState(getData("MA") ?? true)
+      const [MP, setMP] = React.useState(getData("MP") ?? true)
+
+      return React.createElement(React.Fragment, {
+        children: [
+          React.createElement(Switch, {
+            value: MCN,
+            onChange: (value) => {
+              setMCN(value)
+              setData("MCN", value)
+            },
+            children: "Message Context Menu",
+            disabled: !window.ZLibrary
+          }),
+          React.createElement(Switch, {
+            value: mn,
+            onChange: (value) => {
+              setMN(value)
+              setData("MA", value)
+            },
+            children: "Message Actions"
+          }),
+          React.createElement(Switch, {
+            value: MP,
+            onChange: (value) => {
+              setMP(value)
+              setData("MP", value)
+            },
+            children: "Mini Popover"
+          })
+        ]
+      })
+    })
+  }
 }
