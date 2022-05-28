1cbfbbd8ba49ff605b69cda7aff0022ac366c16a
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2022-05-27 17:40:31 -0700

    updater!

--- ReportMessages/ReportMessages.plugin.js
+++ ReportMessages/ReportMessages.plugin.js
@@ -1,11 +1,12 @@
 /**
  * @name ReportMessages
  * @description Report messages within discord. 
- * @version 1.0.0
+ * @version 1.0.1
  * @author doggybootsy
  */
 
 // Modules
+const { React } = BdApi
 const { Messages } = BdApi.findAllModules(e => e.Messages)[1]
 const MiniPopover = BdApi.findModule(m => m.default.displayName === "MiniPopover")
 const Flag = BdApi.findModuleByDisplayName("Flag")
@@ -17,11 +18,77 @@ const { openModal } = BdApi.findModuleByProps("openModalLazy", "openModal")
 const Switch = BdApi.findModuleByDisplayName("SwitchItem")
 const Tooltip = BdApi.findModuleByDisplayName("Tooltip")
 const Markdown = BdApi.findModule((m) => m?.displayName === "Markdown" && m.rules)
-const { React } = BdApi
 // Data
 const getData = BdApi.getData.bind(this, "ReportMessages")
 const setData = BdApi.saveData.bind(this, "ReportMessages")
 const showedPrompt = getData("showedPrompt")
+// update easily
+async function updater(name) {
+  // every 2 hrs run the updater
+  setTimeout(() => updater(name), 1000 * 60 * 60 * 2)
+  // Fetch file
+  const result = await fetch(`https://raw.githubusercontent.com/doggybootsy/BDPlugins/main/${name}/${name}.plugin.js`)
+  const content = await result.text()
+  // Local
+  const meta = BdApi.Plugins.get(name)
+  const out = await new Promise(r => {
+    // Read meta
+    const block = content.split("/**", 2)[1].split("*/", 1)[0]
+    const out = {}
+    let field = "", accum = ""
+    for (const line of block.split(/[^\S\r\n]*?(?:\r\n|\n)[^\S\r\n]*?\*[^\S\r\n]?/)) {
+      if (line.length === 0) continue
+      if (line.charAt(0) === "@" && line.charAt(1) !== " ") {
+        out[field] = accum
+        const l = line.indexOf(" ")
+        field = line.substring(1, l - 1)
+        accum = line.substring(l + 1)
+      } 
+      else accum += " " + line.replace("\\n", "\n").replace(/^\\@/, "@")
+    }
+    out[field] = accum.trim()
+    delete out[""]
+    out.format = "jsdoc"
+    function resolve() {
+      if (out.version) return r(out)
+      setImmediate(() => resolve())
+    }
+    resolve()
+  })
+  // Get versions
+  const onlineVersion = Number(out.version.replaceAll(".", ""))
+  const localVersion = Number(meta.version.replaceAll(".", ""))
+  // if the online version isnt higher return
+  if (!(onlineVersion > localVersion)) return
+  // Open alert asking to update
+  function update() {
+    const path = require("path").join(__dirname, "quickReact.plugin.js")
+    require("fs").writeFileSync(path, content)
+  }
+  if (BdApi.showNotice) BdApi.showNotice(`Plugin update available for ${name}!`, {
+    type: "warning",
+    number: 0,
+    buttons: [{
+      label: "update",
+      onClick: (close) => {
+        close()
+        update()
+      }
+    }]
+  })
+  else {
+    const { openModal } = BdApi.findModuleByProps("openModal", "openModalLazy")
+    const Alert = BdApi.findModuleByDisplayName("Alert")
+    openModal(props => React.createElement(Alert, {
+      ...props,
+      title: name,
+      body: "Plugin is out of date!",
+      cancelText: "Skip",
+      confirmText: "Update",
+      onConfirm: () => update()
+    }))
+  }
+}
 
 module.exports = class ReportMessages {
   getName() { return Messages.REPORT_MESSAGE_MENU_OPTION }
@@ -40,8 +107,8 @@ module.exports = class ReportMessages {
     // show warning
     if (!showedPrompt) this.alert("Spam reporting can get you banned.")
   }
-  // So it doesnt error
   async start() {
+    updater(this.constructor.name)
     // patch 'MiniPopover' and enable the button
     BdApi.Patcher.after("ReportMessages", MiniPopover, "default", (_, args, res) => {
       if (!args[0].children[args[0].children.length - 1]?.props?.message) return 
@@ -64,7 +131,7 @@ module.exports = class ReportMessages {
         }
       }
       const ele = args[0].children[args[0].children.length - 1]
-      if (ele) ele.props.canReport = getData("MCN") ?? true
+      if (ele) ele.props.canReport = getData("MA") ?? true
     })
     // If no zlib return
     if (!window.ZLibrary?.DCM && this.hasStarted) return
