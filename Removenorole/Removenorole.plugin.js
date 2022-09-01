e806bac674e52ff1a1948eb7085b975619927bfb
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2022-08-31 21:44:35 -0700

    Fix

--- Removenorole/Removenorole.plugin.js
+++ Removenorole/Removenorole.plugin.js
@@ -2,27 +2,26 @@
  * @name RemoveNoRole
  * @author Doggybootsy
  * @description Remove the "NO ROLES" from user popouts
- * @version 1.5
+ * @version 1.6
  * @source https://github.com/doggybootsy/BDPlugins/
  * @website https://doggybootsy.github.io/
  */
 
- module.exports = class Remove_no_role{
-    getName() {
-        return "Remove No Role";
-    }
+const classes = Object.assign({}, BdApi.Webpack.getModule(m => m.userPopoutInner && m.userPopoutInner), BdApi.Webpack.getModule(m => m.root && m.addButton))
 
-    observer (mutations) {
-        if (document.querySelector('.userPopout-2j1gM4 .root-jbEB5E:empty')) {
-            document.querySelector('.userPopout-2j1gM4 .root-jbEB5E:empty').previousSibling.style.display = "none";
-            document.querySelector('.userPopout-2j1gM4 .root-jbEB5E:empty').style.display = "none";
-        };
-        if (document.querySelector('.userPopout-2j1gM4 .root-jbEB5E[style]:not(:empty)')) {
-            document.querySelector('.userPopout-2j1gM4 .root-jbEB5E[style]:not(:empty)').previousSibling.removeAttribute('style')
-            document.querySelector('.userPopout-2j1gM4 .root-jbEB5E[style]:not(:empty)').removeAttribute('style')
-        };
-    };
+const addButton = classes.addButton.split(" ")[0]
+const root = classes.root.split(" ")[0]
+const userPopoutInner = classes.userPopoutInner.split(" ")[0]
 
-    start() {};
-    stop() {};
+module.exports = class Remove_no_role {  
+  observer() {
+    const node = document.querySelector(`.${userPopoutInner} .${root}`)
+    if (!node) return
+    const children = Array.from(node.children).filter(m => !m.classList.contains(addButton))
+    if (children.length) return node.parentElement.style.display = ""
+    node.parentElement.style.display = "none"
+  }
+
+  start() {}
+  stop() {}
 }
