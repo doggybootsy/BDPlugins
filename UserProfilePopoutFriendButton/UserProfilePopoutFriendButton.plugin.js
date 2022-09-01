c0257714d63ee08524e56556ab5e34ba5b5c64a2
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2022-08-31 21:46:35 -0700

    Fix for new profiles

--- UserProfilePopoutFriendButton/UserProfilePopoutFriendButton.plugin.js
+++ UserProfilePopoutFriendButton/UserProfilePopoutFriendButton.plugin.js
@@ -6,224 +6,191 @@
  * @updateUrl https://raw.githubusercontent.com/doggybootsy/BDPlugins/main/UserProfilePopoutFriendButton/UserProfilePopoutFriendButton.plugin.js
  */
 
-// Lazy loaded modules
-let UserProfileFriendRequestButton = BdApi.findModuleByDisplayName("UserProfileFriendRequestButton")
-let UserProfileActionsMenu = BdApi.findModuleByDisplayName("UserProfileActionsMenu")
-let classes = BdApi.findModuleByProps("additionalActionsIcon")
-// React
-const { React } = BdApi
-// Modules
-const UserPopout = BdApi.findModuleByProps("UserPopoutInfo")
-const { getCurrentUser } = BdApi.findModuleByProps("getCurrentUser")
-const { addRelationship, removeRelationship } = BdApi.findModuleByProps("addRelationship", "removeRelationship")
-const privateChannel = BdApi.findModuleByProps("openPrivateChannel", "ensurePrivateChannel")
-const OverflowMenu = BdApi.findModuleByDisplayName("OverflowMenu")
-const Clickable = BdApi.findModuleByDisplayName("Clickable")
-const { getRelationshipType } = BdApi.findModuleByProps("getRelationships")
-const dispatch = BdApi.findModuleByProps("dispatch", "dirtyDispatch")
-const { RelationshipTypes } = BdApi.findModuleByProps("RelationshipTypes")
-const { openContextMenu } = BdApi.findModuleByProps("openContextMenuLazy")
-// Catcher Errors
-function reactCatcher(elementType, elementProps) {
-  class reactCatcher extends React.PureComponent {
-    constructor(props) {
-      super(props)
-      this.state = { crashed: false }
-    }
-    static displayName = "UPPFB-Error-Boundary"
-    componentDidCatch() { this.setState({ crashed: true }) }
-    render() {
-      return this.state.crashed ? React.createElement("div", {
-        className: "UPPFB-React-Error"
-      }, "React Error") : React.createElement(elementType, elementProps) 
-    }
-  }
-  return React.createElement(reactCatcher)
-}
-// Make a function component and make it fancy
-function makeFunctionalComponent(type, displayName) {
-  displayName = `UPPFB-${displayName}`
-  type.displayName = displayName + "-Child"
-  return Object.assign((props) => reactCatcher(type, props), { displayName: displayName + "-Wrapper" })
-}
-// A simple wrapper for the relation status
-const RelationStatusWrapper = makeFunctionalComponent(({ user, children }) => {
-  const [relationshipType, setRelationshipType] = React.useState(getRelationshipType(user.id))
-  // Subscribe to relationship add/remove
-  React.useEffect(() => {
-    function onRelationChange({ relationship }) {
-      if (relationship.id === user.id) setRelationshipType(getRelationshipType(user.id))
-    }
-    dispatch.subscribe("RELATIONSHIP_REMOVE", onRelationChange)
-    dispatch.subscribe("RELATIONSHIP_ADD", onRelationChange)
-    return () => {
-      dispatch.unsubscribe("RELATIONSHIP_REMOVE", onRelationChange)
-      dispatch.unsubscribe("RELATIONSHIP_ADD", onRelationChange)
-    }
-  })
-  return children(relationshipType)
-}, "Relation-Status")
+const { React, Webpack, Patcher, ReactDOM, clearCSS, injectCSS } = BdApi
 
-// Add lazy loaded modules if they dont exist
-webpackChunkdiscord_app.push([[Symbol()], {}, async (instance) => {
-  // If the button or the classes arent added load it
-  if (!UserProfileFriendRequestButton || !classes) {
-    await instance.e("16972")
-    UserProfileFriendRequestButton = instance("299993").default
-    classes = instance("674198")
+const RelationshipStore = Webpack.getModule(m => m.getRelationshipType)
+const UserStore = Webpack.getModule(m => m.getCurrentUser && m.getUser)
+const privateChannel = Webpack.getModule(m => m.openPrivateChannel && m.ensurePrivateChannel)
+const { addRelationship, removeRelationship } = Webpack.getModule(m => m.addRelationship && m.removeRelationship)
+const { useStateFromStores } = Webpack.getModule(m => m.useStateFromStores)
+
+const Clickable = Webpack.getModule(m => m.displayName === "Clickable")
+const OverflowMenu = Webpack.getModule(m => m.displayName === "OverflowMenu")
+
+const { section } = Webpack.getModule(m => m.section && m.lastSection)
+  
+const UserPopoutContainer = Webpack.getModule(m => m.type?.displayName === "UserPopoutContainer")
+const ModalApi = Webpack.getModule(m => m.closeModal && m.openModal)
+const MenuApi = Webpack.getModule(m => m.closeContextMenu && m.openContextMenu)
+const { RelationshipTypes } = Webpack.getModule(m => m.RelationshipTypes)
+
+const UserBody = Webpack.getModule(m => {
+  if (!m.default) return
+  const str = m.default.toString()
+  return str.includes(".customStatusActivity") && str.includes(".getUserProfile") && str.includes("usernameIcon:")
+})
+
+const Filters = {
+  UserProfileActionsMenu: m => {
+    if (!m.default) return
+    const str = m.default.toString()
+    return str.includes("AnalyticsLocationProvider") && !str.includes("pendingColors") && !m.default.displayName && !m.LocationContext
+  },
+  UserProfileFriendRequestButton: m => m.displayName === "UserProfileFriendRequestButton",
+  classes: m => m.additionalActionsIcon,
+  otherClasses: m => m.avatar && m.guildMemberProfileTooltipNitroWheel,
+  actionClasses: m => m.pendingIncoming && m.actionButton
+}
+
+const otherClasses = Webpack.getModule(Filters.otherClasses)
+
+const asyncModules = async () => {
+  const cache = {
+    UserProfileActionsMenu: Webpack.getModule(Filters.UserProfileActionsMenu), 
+    UserProfileFriendRequestButton: Webpack.getModule(Filters.UserProfileFriendRequestButton), 
+    classes: Webpack.getModule(Filters.classes)
   }
-  // If 'UserProfileActionsMenu' doesnt exist add it
-  if (!UserProfileActionsMenu) {
-    await instance.e("41056")
-    UserProfileActionsMenu = instance("441056").default
-  }  
-  webpackChunkdiscord_app.pop()
-}])
-// Open the 'UserProfileActionsMenu' Menu
-function openMenu(event, user) {
-  openContextMenu(event, (event) => {
-    return React.createElement(RelationStatusWrapper, {
-      user,
-      children: relationshipType => React.createElement(UserProfileActionsMenu, {
-        ...event, user, relationshipType,
-        onRemoveFriend: () => removeRelationship(user.id),
-        onBlock:() => addRelationship({
+  if (Object.values(cache).filter(m => m).length === 3) return cache
+
+  function fakeRenderAndClick(element, selector) {
+    const node = document.createElement("div")
+    ReactDOM.render(element, node)
+    if (!cache.classes) cache.classes = Webpack.getModule(Filters.classes)
+    if (selector) node.querySelector(selector()).click()
+    ReactDOM.unmountComponentAtNode(node)
+  }
+
+  const undoModal = Patcher.instead("UserProfilePopoutFriendButton", ModalApi, "openModalLazy", (that, [ promise ]) => {
+    const res = promise()
+  
+    return res.then((ReactMemo) => {
+      const str = ReactMemo.toString()
+      if (!(str.includes("user:") && str.includes("autoFocusNote:") && str.includes("friendToken:") && str.includes("initialSection:"))) return ModalApi.openModal(props => React.createElement(ReactMemo, props))
+      undoModal()
+
+      fakeRenderAndClick(React.createElement(ReactMemo), () => `.${cache.classes.relationshipButtons} > :last-child`)
+    })
+  })
+  
+  const undoMenu = Patcher.instead("UserProfilePopoutFriendButton", MenuApi, "openContextMenuLazy", (that, [ event, promise ], orig) => {
+    const res = promise()
+  
+    res.then((ReactMemo) => {
+      const str = ReactMemo.toString()
+      if (!(str.includes("onRemoveFriend:") && str.includes("onMessage:") && str.includes("user:") && str.includes("relationshipType:"))) return orig(event, promise)
+
+      undoMenu()
+      fakeRenderAndClick(React.createElement(ReactMemo))
+    })
+  })
+  
+  fakeRenderAndClick(React.createElement(UserPopoutContainer, {
+    userId: Object.keys(RelationshipStore.getRelationships())[0]
+  }), () => `.${otherClasses.avatar}`)
+  
+  const all = Promise.all([
+    Webpack.waitForModule(Filters.UserProfileFriendRequestButton),
+    Webpack.waitForModule(Filters.UserProfileActionsMenu)
+  ])
+
+  const [ UserProfileFriendRequestButton ] = await all
+
+  return {
+    // Same module id but differnt export than the awaited one
+    UserProfileActionsMenu: Webpack.getModule(Filters.UserProfileActionsMenu), 
+    UserProfileFriendRequestButton, 
+    classes: cache.classes,
+    actionClasses: Webpack.getModule(Filters.actionClasses)
+  }
+}
+
+// Not all modules just the async ones
+const modules = {
+  async: asyncModules()
+}
+modules.async.then(m => Object.entries(m).map(([k,v]) => modules[k] = v))
+window.t = modules
+
+function RequestButton({ user, onClose }) {
+  const { relationshipType, isCurrentUser } = useStateFromStores([ RelationshipStore, UserStore ], () => ({
+    relationshipType: RelationshipStore.getRelationshipType(user.id),
+    isCurrentUser: UserStore.getCurrentUser().id === user.id
+  }))
+
+  return React.createElement("div", {
+    className: `${section} UserProfilePopoutFriendRequest`,
+    children: [
+      React.createElement(modules.UserProfileFriendRequestButton, {
+        isCurrentUser,
+        user,
+        relationshipType,
+        onAddFriend: () => addRelationship({
           userId: user.id,
-          context: { location: "User Profile" },
-          type: RelationshipTypes.BLOCKED
+          context: { location: "User Profile" }
         }),
-        onMessage: async () => {
+        onIgnoreFriend: () => removeRelationship(user.id, { location: "User Profile" }),
+        onSendMessage: async () => {
           await privateChannel.ensurePrivateChannel(user.id)
-          return privateChannel.openPrivateChannel(user.id)
+          privateChannel.openPrivateChannel(user.id)
+          onClose()
         }
-      })
-    })
-  })
-}
-// Request Button
-const RequestButton = makeFunctionalComponent(function({ user }) {
-  // 'UserProfileFriendRequestButton' has a 'isCurrentUser' prop to not show the button
-  const isCurrentUser = getCurrentUser().id === user.id
-  return React.createElement(RelationStatusWrapper, {
-    user, children: relationshipType => React.createElement("div", {
-      className: "UserProfilePopoutFriendRequest",
-      children: [
-        React.createElement(UserProfileFriendRequestButton, {
-          isCurrentUser,
-          user,
-          relationshipType,
-          onAddFriend: () => addRelationship({
-            userId: user.id,
-            context: { location: "User Profile" }
-          }),
-          onIgnoreFriend: () => removeRelationship(user.id, { location: "User Profile" }),
-          onSendMessage: async () => {
-            await privateChannel.ensurePrivateChannel(user.id)
-            return privateChannel.openPrivateChannel(user.id)
-          }
+      }),
+      isCurrentUser ? false : React.createElement(Clickable, {
+        onClick: (event) => MenuApi.openContextMenu(event, (props) => {
+          return React.createElement(Menu, { props, user, onClose })
         }),
-        isCurrentUser ? false : React.createElement(Clickable, {
-          onClick: (event) => openMenu(event, user),
-          role: "button",
-          tabIndex: 0,
-          tag: "div",
-          children: React.createElement(OverflowMenu, {
-            className: classes.additionalActionsIcon
-          })
+        role: "button",
+        tabIndex: 0,
+        tag: "div",
+        children: React.createElement(OverflowMenu, {
+          className: modules.classes.additionalActionsIcon
         })
-      ]
-    })
+      })
+    ]
   })
-}, "RequestButton")
-// update easily
-async function updater(name) {
-  // every 2 hrs run the updater
-  setTimeout(() => updater(name), 1000 * 60 * 60 * 2)
-  // Fetch file
-  const result = await fetch(`https://raw.githubusercontent.com/doggybootsy/BDPlugins/main/${name}/${name}.plugin.js`)
-  const content = await result.text()
-  // If error
-  if (content === "404: Not Found") return
-  // Local
-  const meta = BdApi.Plugins.get(name)
-  const out = await new Promise(r => {
-    // Read meta
-    const block = content.split("/**", 2)[1].split("*/", 1)[0]
-    const out = {}
-    let field = "", accum = ""
-    for (const line of block.split(/[^\S\r\n]*?(?:\r\n|\n)[^\S\r\n]*?\*[^\S\r\n]?/)) {
-      if (line.length === 0) continue
-      if (line.charAt(0) === "@" && line.charAt(1) !== " ") {
-        out[field] = accum
-        const l = line.indexOf(" ")
-        field = line.substring(1, l - 1)
-        accum = line.substring(l + 1)
-      } 
-      else accum += " " + line.replace("\\n", "\n").replace(/^\\@/, "@")
-    }
-    out[field] = accum.trim()
-    delete out[""]
-    out.format = "jsdoc"
-    function resolve() {
-      if (out.version) return r(out)
-      setImmediate(() => resolve())
-    }
-    resolve()
-  })
-  // Get versions
-  const onlineVersion = Number(out.version.replaceAll(".", ""))
-  const localVersion = Number(meta.version.replaceAll(".", ""))
-  // if the online version isnt higher return
-  if (!(onlineVersion > localVersion)) return
-  // Open alert asking to update
-  function update() {
-    const path = require("path").resolve(__dirname, __filename)
-    require("fs").writeFileSync(path, content)
-  }
-  if (BdApi.showNotice) return BdApi.showNotice(`Plugin update available for ${name}!`, {
-    type: "warning",
-    number: 0,
-    buttons: [{
-      label: "update",
-      onClick: (close) => {
-        close()
-        update()
-      }
-    }]
-  })
-  else {
-    const { openModal, closeModal } = BdApi.findModuleByProps("openModal", "openModalLazy")
-    const Alert = BdApi.findModuleByDisplayName("Alert")
-    const id = openModal(props => React.createElement(Alert, {
-      ...props,
-      title: name,
-      body: "Plugin is out of date!",
-      cancelText: "Skip",
-      confirmText: "Update",
-      onConfirm: () => update()
-    }))
-    return () => closeModal(id)
-  }
 }
+
+function Menu({ props, user, onClose }) {
+  const { relationshipType } = useStateFromStores([ RelationshipStore ], () => ({
+    relationshipType: RelationshipStore.getRelationshipType(user.id)
+  }))
+  
+  return React.createElement(modules.UserProfileActionsMenu.default, {
+    ...props, 
+    user, 
+    relationshipType,
+    onRemoveFriend: () => removeRelationship(user.id),
+    onBlock:() => addRelationship({
+      userId: user.id,
+      context: { location: "User Profile" },
+      type: RelationshipTypes.BLOCKED
+    }),
+    onMessage: async () => {
+      await privateChannel.ensurePrivateChannel(user.id)
+      privateChannel.openPrivateChannel(user.id)
+      onClose()
+    }
+  }) 
+}
+
+const css = `.UserProfilePopoutFriendRequest:not(:empty) { display: flex; align-items: center }
+.UserProfilePopoutFriendRequest > :first-child,
+.UserProfilePopoutFriendRequest > .{{pendingIncoming}} > button { width: 100% }
+.UserProfilePopoutFriendRequest > :first-child:last-child { margin-left: auto }`
 
 module.exports = class UserProfilePopoutFriendButton {
   start() {
-    this.stopUpdater = updater(this.constructor.name)
-    // CSS
-    BdApi.injectCSS(this.constructor.name, `.UserProfilePopoutFriendRequest:not(:empty) { display: flex; align-items: center; margin-top: 8px }
-.UserProfilePopoutFriendRequest > :first-child,
-.UserProfilePopoutFriendRequest > .pendingIncoming-3g05VP > button { width: 100% }
-.UserProfilePopoutFriendRequest > :first-child:last-child { margin-left: auto }
-.UPPFB-React-Error { color: red; text-align: center; padding: 6px; margin-top: 10px; background: var(--background-primary); border-radius: 6px }`)
-    // The patch
-    BdApi.Patcher.after(this.constructor.name, UserPopout, "UserPopoutInfo", (_, [{ user }], result) => {
-      result.props.children.push(React.createElement(RequestButton, { user }))
+    Patcher.after("UserProfilePopoutFriendButton", UserBody, "default", (that, [ props ], res) => {
+      let i = res.props.children.indexOf(res.props.children.find(child => child.props.customStatusActivity))
+      if (!~i) i++
+      res.props.children.splice(i + 1, 0, React.createElement(RequestButton, { user: props.user, onClose: props.onClose }))
     })
+
+    if (this.actionClasses) injectCSS("UserProfilePopoutFriendButton", css.replace("{{pendingIncoming}}", this.actionClasses.pendingIncoming))
+    else modules.async.then(m => (this.actionClasses = m.actionClasses) && injectCSS("UserProfilePopoutFriendButton", css.replace("{{pendingIncoming}}", this.actionClasses.pendingIncoming)))
   }
-  async stop() {
-    // Remove css and patch
-    BdApi.clearCSS(this.constructor.name)
-    BdApi.Patcher.unpatchAll(this.constructor.name)
-    void (await this.stopUpdater)()
+  stop() {
+    Patcher.unpatchAll("UserProfilePopoutFriendButton")
+    clearCSS("UserProfilePopoutFriendButton")
   }
 }
