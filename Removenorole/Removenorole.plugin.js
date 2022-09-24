25aa15fb955db594b5a84d5593c35038b9ab8837
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2022-09-24 15:19:38 -0700

    Switching to react instead of dom

--- Removenorole/Removenorole.plugin.js
+++ Removenorole/Removenorole.plugin.js
@@ -2,25 +2,39 @@
  * @name RemoveNoRole
  * @author Doggybootsy
  * @description Remove the "NO ROLES" from user popouts
- * @version 1.7
+ * @version 2.0
  * @source https://github.com/doggybootsy/BDPlugins/
- * @website https://doggybootsy.github.io/
  */
 
-const classes = Object.assign({}, BdApi.Webpack.getModule(m => m.userPopoutInner && m.userPopoutInner), BdApi.Webpack.getModule(m => m.root && m.addButton))
+const { Webpack, Patcher } = BdApi
 
-const addButton = classes.addButton.split(" ")[0]
-const root = classes.root.split(" ")[0]
-const userPopoutInner = classes.userPopoutInner.split(" ")[0]
+const UserBody = Webpack.getModule(m => {
+  if (!m.default) return
+  const str = m.default.toString()
+  return str.includes(".customStatusActivity") && str.includes(".getUserProfile") && str.includes("usernameIcon:")
+})
 
-module.exports = class Remove_no_role {  
-  observer() {
-    const node = document.querySelector(`.${userPopoutInner} .${root}`)
-    if (!node) return 
-    if (node.querySelector(`.${addButton}`)) return node.parentElement.style.display = ""
-    node.parentElement.style.display = "none"
+const PermissionStore = Webpack.getModule(m => m.canManageUser && m.can)
+const { useStateFromStores } = Webpack.getModule(m => m.useStateFromStores)
+
+const { MANAGE_ROLES } = Webpack.getModule(m => m.Permissions?.MANAGE_ROLES).Permissions
+
+module.exports = class { 
+  start() {
+    Patcher.after("RemoveNoRole", UserBody, "default", (that, [ props ], res) => {
+      if (!props.guild || !res) return
+
+      const canAddRoles = useStateFromStores([ PermissionStore ], () => PermissionStore.canManageUser(MANAGE_ROLES, props.user, props.guild))
+
+      const Body = res.props.children.find(child => child.type.render)
+      const RolesSection = Body.props.children.find(child => child?.type?.displayName === "RolesSection")
+
+      if (RolesSection.props.guildMember.roles.length || canAddRoles) return
+
+      Body.props.children.splice(Body.props.children.indexOf(RolesSection), 1)
+    })
   }
-
-  start() {}
-  stop() {}
+  stop() {
+    Patcher.unpatchAll("RemoveNoRole")
+  }
 }
