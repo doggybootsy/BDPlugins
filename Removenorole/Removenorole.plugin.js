a89839b465450847c07b5f72a42027869def06a9
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2023-12-18 16:07:49 -0800

    Fix for SWC -> RSPack

--- Removenorole/Removenorole.plugin.js
+++ Removenorole/Removenorole.plugin.js
@@ -2,42 +2,38 @@
  * @name RemoveNoRole
  * @author Doggybootsy
  * @description Remove the "NO ROLES" from user popouts
- * @version 2.1
+ * @version 2.2
  * @source https://github.com/doggybootsy/BDPlugins/
  */
 
-const { Patcher, Webpack } = new BdApi("RemoveNoRole")
+/** @type {import("betterdiscord").PluginCallback} */
+module.exports = (meta) => {
+  const [ UserRolesList, Flux, PermissionStore, PermissionBits ] = BdApi.Webpack.getBulk({
+    filter: BdApi.Webpack.Filters.byStrings(".default.Messages.ROLES_LIST", ".rolePillBorder]", "disableBorderColor:!0", "return null"),
+    defaultExport: false
+  }, {
+    filter: BdApi.Webpack.Filters.byKeys("useStateFromStores", "Store")
+  }, {
+    filter: BdApi.Webpack.Filters.byStoreName("PermissionStore")
+  }, {
+    filter: (m) => typeof m.MANAGE_ROLES === "bigint",
+    searchExports: true
+  });
 
-const UserBody = Webpack.getModule(m => {
-  if (!m.Z) return
-  const str = m.Z.toString()
-  return str.includes(".customStatusActivity") && str.includes(".getUserProfile") && str.includes("usernameIcon:")
-})
+  return {
+    start() {
+      BdApi.Patcher.after(meta.name, UserRolesList, "default", (that, [ props ], res) => {
+        if (!props.guild || !props.user) return;
 
-const PermissionStore = Webpack.getModule(m => m.Z?.getName?.() === "PermissionStore").Z
-const useStateFromStores = Webpack.getModule(m => m.toString().includes("useStateFromStores"))
+        const canAddRoles = Flux.useStateFromStores([ PermissionStore ], () => PermissionStore.canManageUser(PermissionBits.MANAGE_ROLES, props.user, props.guild));
+        
+        if (props.guildMember.roles.length || canAddRoles) return;
 
-let MANAGE_ROLES
-Webpack.getModule(m => Object.values(m).find((data) => data?.MANAGE_ROLES && (MANAGE_ROLES = data.MANAGE_ROLES)))
-
-module.exports = class { 
-  start() {
-    Patcher.after(UserBody, "Z", (that, [ props ], res) => {
-      if (!props.guild || !res) return
-
-      const canAddRoles = useStateFromStores([ PermissionStore ], () => PermissionStore.canManageUser(MANAGE_ROLES, props.user, props.guild))
-
-      const Body = res.props.children.find(child => child.props.children);
-      const Content = Body.props.children.find(child => child.type.render);
-
-      const RolesSection = Content.props.children.find(child => child?.props?.guildMember && child?.props?.user)
-
-      if (RolesSection.props.guildMember.roles.length || canAddRoles) return
-
-      Content.props.children.splice(Content.props.children.indexOf(RolesSection), 1)
-    })
-  }
-  stop() {
-    Patcher.unpatchAll()
-  }
-}
+        return false;
+      });
+    },
+    stop() {
+      BdApi.Patcher.unpatchAll(meta.name);
+    }
+  };
+};
