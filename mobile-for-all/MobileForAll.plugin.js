4fc25d1ca611a1ed93bf9a4d2268a34638905544
Snues <81967520+Snusene@users.noreply.github.com>
2026-03-01 23:51:33 +0100

    Update MobileForAll.plugin.js

--- mobile-for-all/MobileForAll.plugin.js
+++ mobile-for-all/MobileForAll.plugin.js
@@ -12,13 +12,21 @@ module.exports = (meta) => {
 	const {Patcher} = BdApi;
 	const {Filters, getBulkKeyed, Stores} = BdApi.Webpack;
 
-	const {AvatarMask} = getBulkKeyed({
+	const {AvatarMask, StatusUtils} = getBulkKeyed({
 		AvatarMask: {
 			filter: Filters.bySource(".AVATAR_STATUS_ROUND_40;"),
 			searchDefault: false,
 			map: {
 				SmartAvatar: m => m.type,
 				Avatar: Filters.byStrings("Math.ceil")
+			}
+		},
+
+		StatusUtils: {
+			filter: Filters.bySource("global_name", "isMobile"),
+			searchDefault: false,
+			map: {
+				formatStatus: Filters.byStrings("isMobile", ".STREAMING", ".ONLINE", ".IDLE")
 			}
 		}
 	});
@@ -35,18 +43,31 @@ module.exports = (meta) => {
 				if (args[0]?.isMobile && args[0].status !== "unknown") {
 					args[0] = {
 						...args[0],
+						__mfaStatus: args[0].status,
 						status: "online",
 						isVR: false
 					};
 				}
 			});
 
+			Patcher.after(AvatarMask, "Avatar", (that, [props], ret) => {
+				if (!props.__mfaStatus) return;
+
+				let tooltip = ret?.props?.children?.props?.children?.[3];
+				if (tooltip?.props?.text) {
+					tooltip.props.text = StatusUtils.formatStatus(props.__mfaStatus);
+				}
+			});
+
 			Patcher.after(AvatarMask.SmartAvatar, "type", (that, [props], ret) => {
 				ret.type = AvatarMask.Avatar;
 			});
+
+			Stores.PresenceStore.emitChange();
 		},
 		stop() {
 			Patcher.unpatchAll();
+			Stores.PresenceStore.emitChange();
 		}
 	};
 };
